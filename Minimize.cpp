f52ae367c6ab66fa6b257877e7a81daee7f44dad
mamun795 <mdmamunahmed79500@gmail.com>
2024-09-19 20:43:04 +0600

    CF Problem Minimize

---
+++ Minimize.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#define ll long long
+#define ld long double
+using namespace std;
+#define hmm cout<<"YES"<<endl
+#define na cout<<"NO"<<endl
+#define all(x) x.begin(), x.end()
+
+void solve()
+{
+    ll a,b;
+    cin>>a>>b;
+    cout<<b-a<<endl;
+
+}
+
+
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+
+        solve();
+
+    }
+
+}
+
