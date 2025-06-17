6e08e4c2f022a3d4af84fa190a8f1c956b222cf5
Mamun <mdmamunahmed79500@gmail.com>
2025-06-17 22:23:32 +0600

    Halka Change

--- B_Above_the_Clouds.cpp
+++ B_Above_the_Clouds.cpp
@@ -1,24 +1,44 @@
-#include <bits/stdc++.h>
-#include <cstdint>
-#define int long long
-#define pb push_back
+#include<bits/stdc++.h>
+#include<math.h>
+#define ll long long
 using namespace std;
+#define hmm cout<<"YES"<<endl
+#define na cout<<"NO"<<endl
+#define mod   1000000007
+#define Exp   1e18
 
-int32_t main() {
-	int t;        cin >> t;
-    while(t--){
-        int n;      cin >> n;
+void solve()
+{
+
+    int n;cin >> n;
         string s;        cin >> s;
         map<char,int>m;
         bool ok=false;
         for(int i=1 ; i<n-1 ; i++){
             m[s[i]]++;
             if(m[s[i]]==2 or (s[0]==s[i] ) or s[n-1] == s[i]){
-                ok=true;
+                hmm;
                 break;
             }
         }
-        if(ok)cout<<"Yes"<<endl;
-        else cout<<"No"<<endl;
-    }
+        na;
+        
+
 }
+
+
+int main()
+{
+    ll t;
+    t=1;
+    cin>>t;
+    while(t--)
+    {
+
+        solve();
+
+    }
+
+}
+
+
