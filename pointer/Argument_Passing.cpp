ff7ca6921bca3128857a12bb547fca43d4e3bd49
Mamun <mdmamunahmed79500@gmail.com>
2025-07-10 11:45:06 +0600

    pass by value

---
+++ pointer/Argument_Passing.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define hmm cout<<"YES"<<endl
+#define na cout<<"NO"<<endl
+
+
+
+void modify(ll a){
+    a=100;
+    // it creates a copy of main function 'a';
+    // that's why it can't change
+}
+
+void solve()
+{
+    ll a=50;
+    modify(a);
+    cout<<a<<endl;
+
+
+}
+
+
+int main()
+{
+    ll t;
+    t=1;
+    cin>>t;
+    while(t--)
+    {
+
+        solve();
+
+    }
+
+}
+
+
