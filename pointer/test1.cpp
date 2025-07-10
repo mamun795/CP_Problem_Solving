8f8999568e5e1a62ba69b4803e04baebb8327343
Mamun <mdmamunahmed79500@gmail.com>
2025-07-10 11:00:19 +0600

    Array is one kind of pointer

---
+++ pointer/test1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define hmm cout<<"YES"<<endl
+#define na cout<<"NO"<<endl
+
+void solve()
+{
+    ll a[]={0,1,2,3,4,5};
+    ll *p=a+1;
+    ll *b=p;
+    
+    cout<<b<<' '<<*b<<endl;
+
+
+}
+
+
+int main()
+{
+    ll t;
+    t=1;
+    cin>>t;
+    while(t--)
+    {
+
+        solve();
+
+    }
+
+}
+
+
